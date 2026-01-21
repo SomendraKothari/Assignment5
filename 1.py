f4dba76a125ad7010b2e1257ec15892c91f0fded
SomendraKothari <ch25btech11041@iith.ac.in>
2026-01-21 23:11:07 +0530

    1.py

--- 1.py
+++ 1.py
@@ -1,8 +1,9 @@
 import re
-stu_details = {"Ramesh" : 95 ,"Suresh" : 98 , "Manish" : 94 , "Kamlesh" : 99,}
+stu_details = {"Ramesh" : 95 ,"Suresh" : 98 , "Manish" : 94 , "Kamlesh" : 99}
 name = input("Enter the student's name: ")
 key = re.findall(name,str(stu_details))
 if key :
     print(f"{name}'s mark: {stu_details[key[0]]}")
 else:
+
     print("Student not found")
